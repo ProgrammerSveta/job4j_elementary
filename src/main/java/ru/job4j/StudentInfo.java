07c788dd5ae7c76130fdb623705aa7d5beb1ddff
ProgrammerSveta <leshiarelaohk@gmail.com>
2023-02-16 17:46:33 +0300

    4.1 Добавление Multiple.java

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Sveta");
+        System.out.println("19.04.1989");
+    }
+}
