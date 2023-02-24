54a472f783d2947ed143ec3eba60c52f8e5962a1
ProgrammerSveta <leshiarelaohk@gmail.com>
2023-02-24 15:20:29 +0300

    4.1.2. Глупый бот [#31323]

---
+++ src/test/java/ru/job4j/condition/DummyBotTest.java
@@ -0,0 +1,16 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DummyBotTest {
+
+    @Test
+    public void answer() {
+    }
+
+    @Test
+    public void main() {
+    }
+}
