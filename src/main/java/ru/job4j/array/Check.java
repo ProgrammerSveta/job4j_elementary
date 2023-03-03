649a8bf17e7a0d4532ae0112592d794707a10fe3
ProgrammerSveta <leshiarelaohk@gmail.com>
2023-03-03 13:33:38 +0300

    6.3. Массив заполнен true или false [#53857 #397160]

--- src/main/java/ru/job4j/array/Check.java
+++ src/main/java/ru/job4j/array/Check.java
@@ -7,6 +7,7 @@ public class Check {
         for (int i = 1; i < data.length; i++) {
             if (data[0] != data[i]) {
                 result = false;
+                break;
             }
         }
         return result;
